958ec7ecc241ef25d5628935306f26711ea28710
dkhuriev <khuriev.david@gmail.com>
2018-10-08 01:19:02 +0300

    added new app

---
+++ android_beta/app/build/generated/source/buildConfig/release/com/example/macbook/android_beta/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.macbook.android_beta;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.macbook.android_beta";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
